aa0b9bd1574b1782291dfa848154d8c3bfff137d
Keyla Smith <keyla.r.smith@gmail.com>
2018-07-28 21:42:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,10 @@ def line(a)
   if a.nil? || a.empty?
     puts "The line is currently empty."
   else
-    
+    res = ["The line is currently:"]
+    a.each_with_index do |item, idx|
+      res << "#{idx + 1}. #{item}"
+    end
+    puts res.join(" ")
   end
 end
